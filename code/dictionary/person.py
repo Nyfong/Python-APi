53d94e9dc9b510273c1b00fd971c61be7cad3358
Nyfong <litongfong12@gmail.com>
2024-11-19 11:56:31 +0700

    dictionary

---
+++ code/dictionary/person.py
@@ -0,0 +1,10 @@
+person = {
+    'name': "Pu ket",
+    'age': 22,
+    'major': 'ITE'
+}
+
+
+# Accessing key-value pairs using keys() method
+for key in person.keys():
+	print("Key:", key, "Value:", person[key])

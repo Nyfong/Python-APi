2f1a6082585e7c269c3902ff468a2962c569c76c
Nyfong <litongfong12@gmail.com>
2024-11-19 10:09:06 +0700

    insert

---
+++ code/list/insert.py
@@ -0,0 +1,9 @@
+#list
+your_smallShop = ['banana', 'kdet', 'orange', 'apple','starfruit']
+#insert
+your_smallShop.insert(0,"Kdet base index")
+#appaned
+your_smallShop.append("kdet pi Kroy")
+#display Kdet Item
+for items in your_smallShop:
+    print(items, end=", ")

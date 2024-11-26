e0cb9110ddb515b92a1a9589c4cd8450b797794c
Nyfong <litongfong12@gmail.com>
2024-11-26 09:43:14 +0700

    your_smallAss_companines

--- api-test/app.py
+++ api-test/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from companies import your_small_companies
 app = Flask(__name__)
 
 @app.get("/test") 
@@ -9,36 +9,36 @@ def home():
 #display all companies
 @app.get("/companies") 
 def test():
-	return {"companies": companies}
+	return {"companies": your_small_companies}
 
 
 #creating a company
 @app.post("/company")
 def create_company():
     new_company = {"name": "KESA", "items":[{"name": "pen","price": 15.99}, {"name": "notebook","price": 20}]}
-    companies.append(new_company)
+    your_small_companies.append(new_company)
     return new_company, 201
 
 #get company by name
 @app.get("/company/<string:name>")
 def get_com(name):
-    for company in companies:
+    for company in your_small_companies:
         if company["name"] == name:
             return company
     return {"msg": "Company not found"}, 404
 
-companies = [
-    {
-        "name": "ABSARA",
-        "items": [
-            {
-                "name": "pen",
-                "price": 15.99
-            },
-            {
-                "name": "book",
-                "price": 11
-            }
-        ]
-    }
-]
+# companies = [
+#     {
+#         "name": "ABSARA",
+#         "items": [
+#             {
+#                 "name": "pen",
+#                 "price": 15.99
+#             },
+#             {
+#                 "name": "book",
+#                 "price": 11
+#             }
+#         ]
+#     }
+# ]
---
+++ api-test/companies.py
@@ -0,0 +1,15 @@
+your_small_companies = [
+    {
+        "name": "ABSARA",
+        "items": [
+            {
+                "name": "pen",
+                "price": 15.99
+            },
+            {
+                "name": "book",
+                "price": 11
+            }
+        ]
+    }
+]

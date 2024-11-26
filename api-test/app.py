a35630186c96db9d89264fc3abe58a073f66b6c6
Nyfong <litongfong12@gmail.com>
2024-11-26 09:37:39 +0700

    api flask with POST GET

--- api-test/app.py
+++ api-test/app.py
@@ -6,7 +6,39 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
-@app.get("/aaa") 
-def test(): 
-	return {"msg": "Hello World!"}
+#display all companies
+@app.get("/companies") 
+def test():
+	return {"companies": companies}
 
+
+#creating a company
+@app.post("/company")
+def create_company():
+    new_company = {"name": "KESA", "items":[{"name": "pen","price": 15.99}, {"name": "notebook","price": 20}]}
+    companies.append(new_company)
+    return new_company, 201
+
+#get company by name
+@app.get("/company/<string:name>")
+def get_com(name):
+    for company in companies:
+        if company["name"] == name:
+            return company
+    return {"msg": "Company not found"}, 404
+
+companies = [
+    {
+        "name": "ABSARA",
+        "items": [
+            {
+                "name": "pen",
+                "price": 15.99
+            },
+            {
+                "name": "book",
+                "price": 11
+            }
+        ]
+    }
+]

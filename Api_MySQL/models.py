9e7ac409d9996c25c24b3664250674d75dbe8047
Nyfong <litongfong12@gmail.com>
2024-12-03 11:48:47 +0700

    declare some class for Model

--- Api_MySQL/models.py
+++ Api_MySQL/models.py
@@ -5,3 +5,35 @@ class Terms(db.Model):
     terms_id = db.Column(db.Integer, primary_key=True)
     terms_description = db.Column(db.String(50), nullable=False)
     terms_due_days = db.Column(db.Integer, nullable=False)
+
+
+class Invoices (db.Model):
+    tablename = "invoices"
+
+    invoice_id = db.Column(db.Integer, primary_key=True)
+    # vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.vendor_id'), nullable=False)
+    invoice_number = db.Column(db.Integer, nullable=False)
+    invoice_date = db.Column(db.DateTime, nullable=False)
+    invoice_total = db.Column(db.Float, nullable=False)
+    payment_total = db.Column(db.Float, nullable=False, default=0.00)
+    credit_total  = db.Column(db.Float, nullable=False, default=0.00)
+    terms_id = db.Column(db.Integer, db.ForeignKey('terms.term_id'), nullable=False)
+    invoice_due_date = db.Column(db.DateTime, nullable=False)
+    payment_date = db.Column(db.DateTime, nullable=False, default=None)
+
+class Vendors(db.Model):
+    tablename = "vendors"
+    #logic here
+
+class VendorContacts(db.Model):
+    tablename = "vendor_contacts"
+    #logic here
+
+class InvoiceArchive(db.Model):
+    tablename = "invoice_archive"
+    #logic here
+
+class GeneralLedgerArchive(db.Model):
+    tablename = "general_ledger_archive"
+    #logic here
+    
